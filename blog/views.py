83d72d49d2a20705b45de06f8b1c18254dd01c10
Tydon K <tydonk@gmail.com>
2016-08-04 06:26:24 +0000

    entries per page option added. rules not added yet

--- blog/views.py
+++ blog/views.py
@@ -3,33 +3,36 @@ from flask import render_template, request, redirect, url_for
 from . import app
 from .database import session, Entry
 
-PAGINATE_BY = 10
-
 @app.route("/")
 @app.route("/page/<int:page>")
 def entries(page=1):
+    limit = request.args.get('limit', 10)
+    
+    paginate_by = int(limit)
     # Zero-indexed page
     page_index = page - 1
-    
+
     count = session.query(Entry).count()
-    
-    start = page_index * PAGINATE_BY
-    end = start + PAGINATE_BY
-    
-    total_pages = (count - 1) // PAGINATE_BY + 1
+
+    start = page_index * paginate_by
+    end = start + paginate_by
+
+    total_pages = (count - 1) // paginate_by + 1
     has_next = page_index < total_pages - 1
     has_prev = page_index > 0
-    
+
     entries = session.query(Entry)
     entries = entries.order_by(Entry.datetime.desc())
     entries = entries[start:end]
-    
+
     return render_template("entries.html",
         entries=entries,
         has_next=has_next,
         has_prev=has_prev,
         page=page,
-        total_pages=total_pages
+        total_pages=total_pages,
+        limit=limit,
+        count=count
     )
     
 @app.route("/entry/add", methods=["GET"])
