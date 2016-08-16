917f9b6d5555d8441f5f96dea1663f5a51137a0f
Tydon K <tydonk@gmail.com>
2016-08-16 19:59:23 +0000

    fix Travis CI error

--- blog/config.py
+++ blog/config.py
@@ -1,11 +1,11 @@
 import os
 class DevelopmentConfig(object):
-    SQLALCHEMY_DATABASE_URI = "postgresql://ubuntu:thinkful@localhost:5432/blogful"
+    SQLALCHEMY_DATABASE_URI = "postgresql://localhost:5432/blogful"
     DEBUG = True
     SECRET_KEY = os.environ.get("BLOGFUL_SECRET_KEY", os.urandom(12))
     
 class TestingConfig(object):
-    SQLALCHEMY_DATABASE_URI = "postgresql://ubuntu:thinkful@localhost:5432/blogful-test"
+    SQLALCHEMY_DATABASE_URI = "postgresql://localhost:5432/blogful-test"
     DEBUG = False
     SECRET_KEY = "Not secret"
     
